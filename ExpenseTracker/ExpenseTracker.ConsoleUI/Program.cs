fc6a252fdb65e6c9312782acbf6f2dc393652a57
Samuel Abdias Grehuello <SamuelAbdias.Grehuello@alumnos.uai.edu.ar>
2026-09-22 23:14:01 -0300

    feat: initial clean architecture solution setup

---
+++ ExpenseTracker/ExpenseTracker.ConsoleUI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpenseTracker.ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
